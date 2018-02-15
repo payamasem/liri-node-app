845ed3e5abc1c6497f1539749536d8a7bf60ba46
Payam <payamasem@berkeley.edu>
2018-02-14 22:56:05 -0800

    added spotify key

---
+++ keys.js
@@ -0,0 +1,19 @@
+console.log('this is loaded!');
+
+var twitterKeys = {
+  consumer_key: 'jmZQ5GN1dOLafoxrtm60lDw42',
+  consumer_secret: 	'GSwViN7tpGE7Lfli51fSvutqZLr6sbFaK8BtZocYlEkYBVunTQ',
+  access_token_key: '252677977-ToPnP4aDBYUs1ariFUoGRPW8X7Xb02o0pnqEI5Pl',
+  access_token_secret: 'RrKe4Aa92cu7jOmM1yAmIqcC89wfAu3AHKeElvcSHh3LL'
+}
+
+
+
+var spotifyKeys = {
+  id: "c8be503514674a279e05b0a0942ba46e",
+  secret: "262f9c618c0b439194eb6379977b8b6b"
+};
+
+module.exports = twitterKeys;
+module.exports = spotifyKeys;
+
